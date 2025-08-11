75df6c4ba3f3dfc9a765c990156956334b49c953
frchocolate <chocolateisfr@gmail.com>
2025-08-12 01:48:19 +0330

    Added setrlimit

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,35 @@
 #include "../utils/runner.hpp"
 
 
+
 int main(int argc, char* argv[]){
     const auto chroot = flag("--chroot", argv, argc);
     if (chroot == "NULL") {
         std::cout<<"chroot is NULL, Working without jailing the subsystem."<<std::endl;
     }
-    const Runner sandbox(chroot.c_str());
+
+    u_int64_t msoft;
+    u_int64_t mhard;
+    u_int64_t csoft;
+    u_int64_t chard;
+    const auto memsoft = flag("--memsoft", argv, argc);
+    if (memsoft == "NULL") {
+        std::cout << "WARN: --memsoft is not set" << std::endl;
+
+    }
+    const auto memhard = flag("--memhard", argv, argc);
+    if (memhard == "NULL") {
+        std::cout << "WARN: --memhard is not set" << std::endl;
+    }
+    const auto cpu = flag("--cpu", argv, argc);
+    if (cpu == "NULL") {
+        std::cout << "WARN: --cpu is not set" << std::endl;
+    }
+    const auto cpusoft = flag("--cpusoft", argv, argc);
+    if (cpusoft == "NULL") {
+        std::cout << "WARN: --cpusoft is not set" << std::endl;
+    }
+    const Runner sandbox(chroot.c_str(), memsoft, memhard, cpusoft, cpu);
     const auto cmd  = flag("--cmd", argv, argc);
     std::cout << cmd << std::endl;
     sandbox.run(cmd);
--- utils/runner.hpp
+++ utils/runner.hpp
@@ -1,18 +1,81 @@
 #ifndef RUNNER_H
 #define RUNNER_H
 
+#include <unistd.h>
+#include <iostream>
+#include <cstdlib>
 #include <string>
-#include <unistd.h>   // chroot, chdir, execl
-#include <iostream>   // std::cerr
-#include <cstdlib>    // exit, EXIT_FAILURE
-#include <cstdio>     // perror
-
+#include <utility>
+#include <sys/resource.h>
+#include <cerrno>
+#include <cstring>
 class Runner {
 public:
-    explicit Runner(const char* jr) : jailRoot(jr) {}
+    const char* jailRoot;
+    const std::string memory;
+    const std::string  memoryHard;
+    const std::string  cpu;
+    const std::string  cpuHard;
+    explicit Runner(const char* jr, std::string mem, std::string memh, std::string cpu, std::string cpuh)
+    : jailRoot(jr), memory(std::move(mem)), memoryHard(std::move(memh)),
+      cpu(std::move(cpu)), cpuHard(std::move(cpuh)) {}
+
+   void limit_resources() const {
+
+    auto to_rlim = [](const std::string& s) -> rlim_t {
+        if (s == "NULL") return RLIM_INFINITY;
+        try {
+            return std::stoull(s);
+        } catch (...) {
+            return RLIM_INFINITY;
+        }
+    };
+
+
+    rlim_t mem_soft = to_rlim(memory);
+    rlim_t mem_hard = to_rlim(memoryHard);
+    rlim_t cpu_soft = to_rlim(cpu);
+    rlim_t cpu_hard = to_rlim(cpuHard);
+
+    mem_soft *= 1024 * 1024;
+    mem_hard *= 1024 * 1024;
+
+    if (cpu_soft != RLIM_INFINITY && cpu_hard != RLIM_INFINITY) {
+        if (cpu_soft > cpu_hard) {
+            std::cerr << "[WARN] CPU soft limit > hard limit; adjusting soft to hard.\n";
+            cpu_soft = cpu_hard;
+        }
+        struct rlimit rl{};
+        rl.rlim_cur = cpu_soft;
+        rl.rlim_max = cpu_hard;
+        std::cerr << "[DEBUG] Setting RLIMIT_CPU soft=" << rl.rlim_cur << " hard=" << rl.rlim_max << std::endl;
+        if (setrlimit(RLIMIT_CPU, &rl) != 0) {
+            std::cerr << "[ERROR] setrlimit CPU failed: " << strerror(errno) << " (errno=" << errno << ")\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (mem_soft != RLIM_INFINITY && mem_hard != RLIM_INFINITY) {
+        if (mem_soft > mem_hard) {
+            std::cerr << "[WARN] Memory soft limit > hard limit; adjusting soft to hard.\n";
+            mem_soft = mem_hard;
+        }
+        struct rlimit rl{};
+        rl.rlim_cur = mem_soft;
+        rl.rlim_max = mem_hard;
+        std::cerr << "[DEBUG] Setting RLIMIT_AS soft=" << rl.rlim_cur << " hard=" << rl.rlim_max << std::endl;
+        if (setrlimit(RLIMIT_AS, &rl) != 0) {
+            std::cerr << "[ERROR] setrlimit AS failed: " << strerror(errno) << " (errno=" << errno << ")\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+}
 
     void run(const std::string& cmd) const {
-        if (jailRoot != nullptr) {
+        std::cerr << "--" << memory << "\n";
+
+        if (jailRoot != nullptr && std::string(jailRoot) != "NULL") {
+            std::cout << jailRoot << std::endl;
             if (chroot(jailRoot) != 0) {
                 perror("chroot failed");
                 exit(EXIT_FAILURE);
@@ -22,15 +85,14 @@ public:
                 exit(EXIT_FAILURE);
             }
         }
-        execl("/bin/sh", "sh", "-c", cmd.c_str(), nullptr);
-
-        // If execl returns, it failed
+        if (memory != "NULL" || memoryHard != "NULL" || cpu != "NULL" || cpuHard != "NULL") {
+            std::cout << "Limiting resources" << std::endl;
+            limit_resources();
+        }
+        execl(cmd.c_str(), cmd.c_str(), nullptr);
         perror("execl failed");
         exit(EXIT_FAILURE);
     }
-
-private:
-    const char* jailRoot;
 };
 
 #endif // RUNNER_H
